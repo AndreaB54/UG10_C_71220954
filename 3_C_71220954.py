1704c80e6cc41bdf2e4c9d835cd9d6f565c58adc
AndreaB54 <117893335+AndreaB54@users.noreply.github.com>
2022-11-24 19:01:11 +0900

    Delete file

--- 3_C_71220954.py
+++
@@ -1,8 +0,0 @@
-Daftar = input("Masukkan daftar pesanan : ")
-pesanan = Daftar.split(',',4)
-print (f"Daftar pesanan : {pesanan}")
-Tambahan = input("Masukkan pesanan yang ingin ditambahkan : ")
-pesanan.append(Tambahan)
-print(f"Hasil penambahan pada daftar pesanan : {pesanan}")
-
-
